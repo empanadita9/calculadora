98f0e2d13a30ae6aea1001230f4780c4fe5e4b05
max garcia <maxrgr1307@gmail.com>
2022-04-15 18:38:06 -0500

    los numero ahora se visualizan en el html

--- script-cal.js
+++ script-cal.js
@@ -5,6 +5,7 @@ for (let i = 0; i < allbtn.length; i++) {
 
 let calc = document.querySelector("#calculated");
 let result = document.querySelector("#result");
+let signal_igual = document.querySelector("#signaligual");
 
 const lisNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 const listOperator = ["+", "-", "*", "/"];
@@ -15,12 +16,13 @@ let listResult = ["123", "-", "673", "*", "2"];
 let a = ""
 let b = ""
 let op = ""
-let total = 0
+let total = ""
+let historial = ""
 
 function Writting() {
   const value = this.getAttribute("value");
-
   if (lisNumber.find((list) => list == value)) {
+    calc.innerHTML = value
     op != "" && a != "" ? (b += value, opera(op)) : a += value    
   } else if (listOperator.find((list) => list == value)) {
     a && b && op ? (a = total, reset() ) : ""
@@ -28,11 +30,15 @@ function Writting() {
   } else {
     console.log("no validado number or operation", value);
   }
+  historial += value
   console.log(`${a}${op}${b} = ${total}`)
+  calc.innerHTML = historial;
+  result.innerHTML = total;
+  total == ""? result.innerHTML = a :   signal_igual.innerHTML = "="
 }
 
 function opera(op) {
-  total = 0
+  total = ""
   let array = {
     "+": parseInt(a) + parseInt(b),
     "-": parseInt(a) - parseInt(b),
@@ -44,5 +50,5 @@ function opera(op) {
 function reset() {
   b = ""
   op = ""
-  total = 0
+  total = ""
 }
